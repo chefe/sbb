323434856b8b8d0ddae5d4aa74f621f0f1f636cc
chefe <hi@chefe.io>
2020-12-24 10:06:15 +0100

    Show delays and platform changes

--- src/widgets/section.rs
+++ src/widgets/section.rs
@@ -1,3 +1,4 @@
+use chrono::offset::FixedOffset;
 use chrono::DateTime;
 use gtk::prelude::*;
 
@@ -18,7 +19,10 @@ impl SectionWidget {
         container.attach(&departure_location, 1, 0, 1, 1);
 
         let departure_time = Self::create_label_with_default_margin();
-        departure_time.set_markup(&Self::get_formatted_time(&section.departure.departure));
+        departure_time.set_markup(&Self::format_time_with_delay(
+            &section.departure.departure,
+            &section.departure.delay,
+        ));
         container.attach(&departure_time, 0, 0, 1, 1);
 
         let departure_platform = Self::create_platform_label(&section.departure.platform);
@@ -30,7 +34,10 @@ impl SectionWidget {
         container.attach(&arrival_location, 1, 2, 1, 1);
 
         let arrival_time = Self::create_label_with_default_margin();
-        arrival_time.set_markup(&Self::get_formatted_time(&section.arrival.arrival));
+        arrival_time.set_markup(&Self::format_time_with_delay(
+            &section.arrival.arrival,
+            &section.arrival.delay,
+        ));
         container.attach(&arrival_time, 0, 2, 1, 1);
 
         let arrival_platform = Self::create_platform_label(&section.arrival.platform);
@@ -67,6 +74,13 @@ impl SectionWidget {
 
         let label = Self::create_label_with_default_margin();
         label.set_markup(&format!("Pl. {}", platform));
+
+        if platform.ends_with("!") {
+            let mut platform = platform.to_owned();
+            platform.pop();
+            label.set_markup(&format!("<span foreground=\"red\">Pl. {}</span>", platform));
+        }
+
         label
     }
 
@@ -79,17 +93,36 @@ impl SectionWidget {
         label
     }
 
-    fn get_formatted_time(input: &Option<String>) -> String {
+    fn format_time_with_delay(time: &Option<String>, delay: &Option<u16>) -> String {
+        let time = match Self::parse_time(time) {
+            Some(date) => date.format("%H:%M").to_string(),
+            None => return "".to_owned(),
+        };
+
+        let delay: u16 = match delay {
+            Some(val) => *val,
+            None => 0,
+        };
+
+        let color = match delay {
+            0 => "gray",
+            _ => "red",
+        };
+
+        return format!("{} <span foreground=\"{}\">+{}</span>", time, color, delay);
+    }
+
+    fn parse_time(input: &Option<String>) -> Option<DateTime<FixedOffset>> {
         let format = "%Y-%m-%dT%H:%M:%S%z";
 
         let time = match input.as_ref() {
             Some(time) => time,
-            None => return "".to_owned(),
+            None => return None,
         };
 
         match DateTime::parse_from_str(time, format) {
-            Ok(rfc3339) => rfc3339.format("%H:%M").to_string(),
-            Err(_) => "".to_owned(),
+            Ok(rfc3339) => Some(rfc3339),
+            Err(_) => None,
         }
     }
 }
